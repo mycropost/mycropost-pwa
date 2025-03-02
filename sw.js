93265f4b27dc453f9b228fc0dedbb19c613df8b1
The Mycropost <102516931+mycropost@users.noreply.github.com>
2025-03-02 20:54:27 +0300

    Create sw.js
    The Mycropost App

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mycropost-cache-v1';
+const urlsToCache = [
+  '/',
+  '/?source=pwa',
+  '/p/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
